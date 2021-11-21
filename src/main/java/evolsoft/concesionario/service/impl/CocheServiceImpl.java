5b0e0d78d51eb06b8342b81c4fc5fe41d958807e
Eliseo Fernandez <eliseo.fernandezmonroy@alum.uca.es>
2021-11-21 18:00:38 +0100

    Añadida implementación de findCochesInPriceRange

--- src/main/java/evolsoft/concesionario/service/impl/CocheServiceImpl.java
+++ src/main/java/evolsoft/concesionario/service/impl/CocheServiceImpl.java
@@ -102,5 +102,15 @@ public class CocheServiceImpl implements CocheService {
 		});
 		return cochesInStock;
 	}
+	
+	@Override
+	public List<CocheDTO> findCochesInPriceRange(Integer minPrice, Integer maxPrice){
+	List<Coche> coches = cocheDAO.findCarsBtwPriceRange(minPrice, maxPrice);
+	final List<CocheDTO> cochesInRange = new ArrayList<>();
+	coches.forEach(coche -> {
+		final CocheDTO cocheDTO = map(coche);
+		cochesInRange.add(cocheDTO);
+		});
+	return cochesInRange;}
 
 }
